6e4edc5cf35dcef58f5284dcd2cf8a585df49f83
WanderAI <shreeyapatro17103@gmail.com>
2025-02-08 00:17:23 +0530

    Revert "Added Frontend Files"
    This reverts commit 3df1ac1857022184823e8bc394b2ad909073a29b.

--- TechXcel/Tourly/extra/FAQ/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const faqs = document.querySelectorAll(".faq");
-
-    faqs.forEach(faq => {
-        faq.querySelector(".faq-question").addEventListener("click", () => {
-            faq.classList.toggle("active");
-        });
-    });
-});
