acc96385859639d6c8334369a6c80d5e0c3f1eef
shreemankumar <bshreemankumar@gmail.com>
2025-02-08 00:45:47 +0530

    1st commit

---
+++ Hack_Halden/app/src/main/java/com/example/invisonaries/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.invisonaries
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.invisonaries.ui.theme.InvisonariesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        installSplashScreen()
+        setContent {
+            InvisonariesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
---
+++ Hack_Halden/app/src/main/java/com/example/invisonaries/MediaPlayerHelper.kt
@@ -0,0 +1,26 @@
+package com.example.invisonaries
+
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class MediaPlayerHelper(context: Context) {
+    private val mediaPlayer: MediaPlayer = MediaPlayer.create(context, R.raw.loading)
+
+    fun play() {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+        }
+    }
+
+    fun stop() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+            mediaPlayer.prepare() // Prepare the media player for the next play
+        }
+    }
+
+    fun release() {
+        mediaPlayer.release()
+    }
+}
