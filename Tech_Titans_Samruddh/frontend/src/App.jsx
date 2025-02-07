fd07ad9d56f06d278a8a972b0d33cc05f8be06b7
SAmmyuxh <samruddhshubhadarshi@gmail.com>
2025-02-08 03:48:51 +0530

    Classifier

--- Tech_Titans_Samruddh/frontend/src/App.jsx
+++ Tech_Titans_Samruddh/frontend/src/App.jsx
@@ -9,6 +9,7 @@ import RecyclingFacilities from "./components/RecyclingFacilities"
 import AboutUs from "./components/AboutUs"
 import Checkout from './components/Checkout';
 import Success from "./components/Success"
+import Classifier from "./components/Classifier"
 function App() {
   
 
@@ -19,6 +20,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/classifier" element={<Classifier/>}/>
       <Route path="/checkout" element={<Checkout/>}/>
       <Route path="/success" element={<Success />} />
       <Route path="/marketplace" element={<Marketplace />} />
